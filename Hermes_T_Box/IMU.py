a180599fc63ccf8c505e51da02f32ca7d0fde3f5
norahotes <56563200+norahotes@users.noreply.github.com>
2020-01-30 15:10:04 -0800

    Add files via upload

---
+++ Hermes_T_Box/IMU.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from math import degrees
+from time import sleep
+import RTIMU
+#SETTINGS_FILE = "/home/pi/displacedRealityDrone/IMU/RTEllipsoidFit/RTIMULib.ini"
+SETTINGS_FILE = "/home/pi/SeniorDesign_159/Hermes_T_Box/IMU/RTEllipsoidFit/RTIMULib.ini"
+s = RTIMU.Settings(SETTINGS_FILE)
+imu = RTIMU.RTIMU(s)
+if (not imu.IMUInit()):
+  print("Failed to initialize IMU")
+  exit(1)
+else:
+  print("Recodring IMU Data")
+imu.setSlerpPower(0.02)
+imu.setGyroEnable(True)
+imu.setAccelEnable(True)
+imu.setCompassEnable(True)
+poll_interval = imu.IMUGetPollInterval()
+while True:
+  if imu.IMURead():
+    data = imu.getIMUData()
+    fusionPose = data["fusionPose"]
+    global roll, pitch, yaw
+    roll = degrees(fusionPose[0])
+    pitch = degrees(fusionPose[1])
+    yaw = degrees(fusionPose[2])
+    print(str(roll) + "   " + str(pitch) + "   " + str(yaw))
+    sleep(0.2)
